0a87d61e65cbce681506d8ddbf8e117cea1858d0
Qingqing Yang <qingqingyang@hotmail.co.uk>
2024-01-27 15:53:31 +0000

    add on click event associated with the search button

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-//API key
+// API key
 var APIKey = "79855f5e2cc5e0d576620f20c250c98a";
-
-// URL query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
-
 
 var currentDate = dayjs().format('DD/MM/YYYY');
 var nextDay = dayjs().add(1, 'day').format('DD/MM/YYYY');
@@ -13,7 +9,26 @@ console.log(nextDay);
 
 var searchHistory = [];
 
-//Creat function display the weather (search button or )
-function diaplayWeather() {
 
+
+//Dispaly the weather (after click search button or city buttons)
+function diaplayWeather(Data) {
+  console.log(Data);
 };
+
+// On click event associated with the Search Button
+$('#search-button').on('click', function (event) {
+  event.preventDefault();
+
+  var inputCity = $('#search-input').val().trim();
+  console.log(inputCity);
+
+  // URL query the database
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + APIKey;
+
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(diaplayWeather);
+});
